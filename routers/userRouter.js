062ae1d33ae300374de715ca97df8cce281ed847
arshullah <ullaharshullah@gmail.com>
2025-03-21 07:07:14 +0530

    new

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,16 +1,38 @@
 const express = require('express');
+const Model = require('../models/userModel');
 
 // router banao jo main file ke sath me lgta hai
 const router = express.Router();
 
-router.get('/add', (req,res) => {
-    res.send('response from user add');
+router.post('/add', (req,res) => {
+
+console.log(req.body);
+
+
+    new Model(req.body).save()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        
+    });
 });
 
 // getall
 router.get('/getall', (req,res) => {
-    res.send('response from user getall');
+    Model.find()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
+
+router.get('/getbyemail', (req ,res)=>{
+    res.send('response from user getbye,ai')
+})
 // getbyid
 router.get('/getyid', (req,res) => {
     res.send('response from user getbyid');
